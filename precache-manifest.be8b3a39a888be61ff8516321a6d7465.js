1a4339f4c76107e27ed72c65ea92095d5a7c0d7c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-14 23:23:26 +0000

    Deploy Bilits/comp3104-week02-wed to github.com/Bilits/comp3104-week02-wed.git:gh-pages

---
+++ precache-manifest.be8b3a39a888be61ff8516321a6d7465.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9b1eb843ba33371d0e3",
+    "url": "/static/css/main.d9cfb027.chunk.css"
+  },
+  {
+    "revision": "b9b1eb843ba33371d0e3",
+    "url": "/static/js/main.b9b1eb84.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "88efe1c7ccddc4ac74e8f92d8767c609",
+    "url": "/index.html"
+  }
+];
